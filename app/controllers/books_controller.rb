294bb96eb835a40fb3f4b8cd51863b7d3ee5e3a4
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-09-07 15:20:43 +0000

    [finish]3-20

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books/index' => 'books#index'
+  get 'books/:id' => 'books#show',as:'list'
+  get 'books/edit'
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
